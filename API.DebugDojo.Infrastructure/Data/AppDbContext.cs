883b68f2f063e961662abb220f6034a98b80d26d
JordiPC95 <jprunellcampos@gmail.com>
2026-01-04 11:58:40 +0100

    actu

--- API.DebugDojo.Infrastructure/Data/AppDbContext.cs
+++ API.DebugDojo.Infrastructure/Data/AppDbContext.cs
@@ -37,7 +37,7 @@ public sealed class AppDbContext : DbContext
              .HasForeignKey(x => x.OwnerUserId)
              .OnDelete(DeleteBehavior.Cascade);
 
-            b.HasQueryFilter(x => !x.IsDeleted); // Soft deletee globall
+            b.HasQueryFilter(x => !x.IsDeleted); // Cambios a full
         });
     }
 }
